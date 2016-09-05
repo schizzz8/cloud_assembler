a159a98b8b9ead922d9900168dc5c36342f5a504
Federico <fenardi87@gmail.com>
2016-09-05 11:01:59 +0200

    using txt_io writer

--- src/cloud_assembler_node.cpp
+++ src/cloud_assembler_node.cpp
@@ -4,6 +4,7 @@
 #include <string>
 
 #include <txt_io/message_reader.h>
+#include <txt_io/message_writer.h>
 #include <txt_io/pinhole_image_message.h>
 #include "globals/system_utils.h"
 
@@ -24,13 +25,10 @@ using namespace std;
 using namespace txt_io;
 using namespace std;
 
-typedef pcl::PointXYZ PointT;
-typedef pcl::PointCloud<PointT> PointCloud;
-
 
 int main(int argc, char ** argv) {
 
-    int count = 20;
+    int count = 2000;
     string pose_stamp;
     string line;
     ifstream trajectories("CameraTrajectory.txt");
@@ -39,16 +37,10 @@ int main(int argc, char ** argv) {
     MessageReader reader;
     reader.open("orazio_2016_run.txt");
 
-    BaseMessage* msg = 0;
+    MessageWriter writer;
+    writer.open("orazio_2016_run_corrected.txt");
 
-    Eigen::Matrix3f K;
-    K << 285.171, 0, 160,
-                    0, 285.171, 120,
-                    0, 0, 1;
-    Eigen::Matrix3f iK = K.inverse();
-    int rows=0,cols=0;
-    float depth_scale;
-    PointCloud::Ptr aggregate_cloud (new PointCloud ());
+    BaseMessage* msg = 0;
 
     if(trajectories.is_open() && associations.is_open()) {
         while (count > 0) {
@@ -62,7 +54,7 @@ int main(int argc, char ** argv) {
             trajectories >> qz;
             trajectories >> qw;
             Eigen::Vector3f t(x,y,z);
-            Eigen::Quaternion<float> q(qx,qy,qz,qw);
+            Eigen::Quaternion<float> q(qw,qx,qy,qz);
 
             string rgb_stamp,rgb_filename,depth_stamp,depth_filename;
             do {
@@ -75,43 +67,23 @@ int main(int argc, char ** argv) {
             } while(pose_stamp.find(rgb_stamp) == string::npos);
 
             bool found = false;
-            cv::Mat depth_image;
             while(found == false) {
                 msg = reader.readMessage();
+                msg->untaint();
                 BaseImageMessage* img = dynamic_cast<BaseImageMessage*> (msg);
                 if(img)
                     if(strcmp(img->topic().c_str(),"/camera/depth/image_raw") == 0 && atof(depth_stamp.c_str())-img->timestamp() == 0) {
-                        depth_image = img->image();
-                        rows = depth_image.rows;
-                        cols = depth_image.cols;
-                        depth_scale = img->depthScale();
+                        img->setOffset(Eigen::Isometry3f::Identity());
+                        Eigen::Isometry3f transform = Eigen::Isometry3f::Identity();
+                        transform.translation() = t;
+                        transform.rotate (q);
+                        img->setOdometry(transform);
                         found = true;
-                    }
+                    }   
             }
-
-            PointCloud::Ptr temp_cloud (new PointCloud ());
-            for(size_t r=0; r<rows; r++) {
-                const unsigned short* id_ptr = depth_image.ptr<unsigned short>(r);
-                for(size_t c=0; c<cols; c++) {
-                    unsigned short id = *id_ptr;
-                    float d = id*depth_scale;
-                    Eigen::Vector3f p = iK*Eigen::Vector3f(c*d,r*d,d);
-                    temp_cloud->push_back(PointT (p.x(),p.y(),p.z()));
-                    id_ptr++;
-                }
-            }
-
-            Eigen::Affine3f transform = Eigen::Affine3f::Identity();
-            transform.translation() = t;
-            transform.rotate (q);
-            PointCloud::Ptr transformed_cloud (new PointCloud ());
-            pcl::transformPointCloud(*temp_cloud,*transformed_cloud,transform);
-
-            *aggregate_cloud += *transformed_cloud;
+            writer.writeMessage(*msg);
             count--;
         }
     }
-    pcl::io::savePCDFileASCII ("test_pcd.pcd", *aggregate_cloud);
-
     return 0;
 }
